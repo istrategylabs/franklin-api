b8c8bad1cb37c740ee55ed8f7e31da4305b3c166
Julian Gindi <juliangindi@gmail.com>
2015-07-19 20:40:19 -0400

    working postgres locally, static dir

--- franklin/config/settings.py
+++ franklin/config/settings.py
@@ -78,9 +78,10 @@ WSGI_APPLICATION = 'franklin.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.environ['DB_NAME'],
+        "HOST": os.environ['DB_HOST'],
     }
 }
 
