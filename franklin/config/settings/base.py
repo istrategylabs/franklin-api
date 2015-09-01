300765562dce4979adfda864ad51d53576bf85cf
Julian Gindi <juliangindi@gmail.com>
2015-09-01 17:59:58 -0400

    converting secret_key to environment variable

--- franklin/config/settings/base.py
+++ franklin/config/settings/base.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9y#npi$wg*@w)64et(i62b_y4fp4gm5+kv$wvi08zm#uwqp1)p'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
